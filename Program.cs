739409dd416607d0a186db3bd1c041a135d99f72
Amir Alam <sharpprogrammer2018@gmail.com>
2023-09-13 20:18:59 +0530

    s

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿public class Bingo {
-    public static void main(string[] args) {
-        if(args.Length > 1){
-            
+﻿using System.IO;
+
+public class Bingo
+{
+    public static void main(string[] args)
+    {
+        if (args.Length > 1)
+        {
+            Environment.Exit(0);
         }
+        else if (args.Length == 1)
+        {
+            runFile(args[0]);
+        }
+    }
+
+    static void runFile(string path)
+    {
+        byte[] content = File.ReadAllBytes(path);
+        string stringCont = System.Text.Encoding.UTF8.GetString(content);
     }
 }
